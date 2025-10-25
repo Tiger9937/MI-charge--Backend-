c9c287f90a9851ee1a1fac1e6acbdf3087c185dc
jagannath sahoo <jagannathsahoo367900@gmail.com>
2025-10-25 17:48:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/Login' , (res , req)=>{
+    res.send('Hello World!')
+})
+
+
+console.log(process.env.PORT)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
